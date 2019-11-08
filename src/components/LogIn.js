e5a23facfd078b33d684cfbbb4fd7bd676be077b
승가리 <imsa1224@korea.ac.kr>
2019-11-08 23:21:53 +0900

    nav바 디자인 LogIn 컴포넌트 만듦(필요한지는 모르겠음)

---
+++ src/components/LogIn.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function LogIn() {
+  return (
+    <div>로그인</div>
+  );
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,68 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import h from 'img/nav/h.png';
+import x from 'img/nav/x.png';
 
 export default function Nav() {
+  const [open, setOpen] = useState(false);
+  const onToggle = () => setOpen(!open);
   return (
     <NavStyle>
-      여기가 네브바를 컨트롤 해야하는 부분이야
+      <NavButton onClick={onToggle} open={open}> 
+        {!open ? <img src={h} alt="menu button" className="h"></img> : <img src={x} alt="menu button" className="x"></img>}
+      </NavButton>
+      <NavMenu>
+          {open && (
+              <div className="menus">
+                <ul>
+                  <li><Link to="LogIn">로그인</Link></li>&nbsp;&nbsp;
+                  <li><Link to="EventList">이벤트페이지</Link></li>
+                </ul>
+              </div>    
+            )}
+        </NavMenu>
     </NavStyle>
+
   );
 }
-
 const NavStyle = styled.div`
-  position: fixed;
+    background: #9F6B36;
+    opacity: 0.8;
+    display: flex;
+    flex-direction: row-reverse;
+    position: absolute;
+    right: 30px;
+    top: 20px;
+    align-content: center;
+    align-items: center;
+    border-radius: 20px 20px;
+    
+`;
+const NavButton = styled.div`
+    width: 40px;
+    height: 40px;
+    background: #714317;
+    border-radius: 50%;
+    cursor: pointer;
+    text-align: center;
+    .h, .x{
+      margin-top: 27%;
+    }
+`;
+const NavMenu = styled.div`
+  .menus ul{
+      list-style: none;
+      margin: 0 10px 0 15px;
+      padding: 0;
+    }
+    .menus li{
+      margin:0;
+      display: inline;
+    }
+    .menus a{
+      text-decoration: none;
+      color: #EDE6D1;
+    }
 `;
