927d9c081b0fb808b2841fdba73f83a522bada0e
Dody Kang <kangdoroth2500@gmail.com>
2019-11-22 01:24:25 +0900

    fixed landing button image

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 import button from 'img/landing/button.png';
+import buttonPush from 'img/landing/buttonPush.png';
 import sauce from 'img/landing/sauce.png';
 
-export default function Landing() {
+export default function Landing({ history }) {
+  const [hover, setHover] = useState(false);
   return (
     <LandingStyle>
       <div className="content">
         <img src={sauce} alt="sauce" className="sauce" />
-        <div className="main">토핑만<br />고르세요</div>
-        <div className="sub">좋아하는 토핑을 골라 도우에 올려놓으세요.<br />선택한 토핑에 맞는 피자를 보여드립니다 </div>
-        <Link to="selectTopping">
-          <div className="btnWrapper">
-            <img src={button} alt="start button" />
-            {/* <div className="text">START</div> */}
-          </div>
-        </Link>
-
+        <div className="relative" style={{ zIndex: 1 }}>
+          <div className="main">토핑만<br />고르세요</div>
+          <div className="sub">좋아하는 토핑을 골라 도우에 올려놓으세요.<br />선택한 토핑에 맞는 피자를 보여드립니다 </div>
+          <Link to="selectTopping">
+            <div className="btnWrapper" onMouseOver={() => setHover(true)} onFocus={() => setHover(true)} onMouseLeave={() => setHover(false)}>
+              {hover ? <img src={buttonPush} alt="start button" />
+                : <img src={button} alt="start button" />}
+            </div>
+          </Link>
+        </div>
       </div>
     </LandingStyle>
   );
@@ -49,6 +52,7 @@ const LandingStyle = styled.div`
     }
     .sub{
       line-height: 1.5;
+      margin-bottom: 5rem;
     }
     .btnWrapper{
       width: 150px;
@@ -56,7 +60,7 @@ const LandingStyle = styled.div`
       display: inline-block;
       img{
         width: 150px;
-        margin-top: 5rem;
+        height: 150px;
       }
       .text{
         margin-top: -95px;
