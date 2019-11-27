f2e86ca1d33c8029217ee62db80972be10971c6f
Dody Kang <kangdoroth2500@gmail.com>
2019-11-28 03:17:30 +0900

    added redux setting

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom';
 import rootReducer from './modules';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools} from 'redux-devtools-extension'
 
 import './styles/main.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-const store = createStore(rootReducer, composeWithDevTools());
+  
+// 개발모드일때만 보이게 합니다.
+const isDev = process.env.NODE_ENV === 'development';
+const devtools = isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancer = devtools || compose;
+const store = createStore(rootReducer, composeEnhancer());
 
 ReactDOM.render( 
   <Provider store={store}>
