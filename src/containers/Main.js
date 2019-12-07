43b846def3296dddf9bdb58edc67505b8a2bd2ae
Dody Kang <kangdoroth2500@gmail.com>
2019-12-07 15:51:10 +0900

    added lazy, suspense at main page for router setting

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,19 +1,19 @@
 // router를 세팅하는 곳 입니다.
 // lazy는 처음에 랜딩 되는 페이지만 import 해서 보여준다. 그 페이지 들어가기 전까지 로드 안해줘
 // Suspense 처음 로딩될때 로딩되는 페이지를 추가해준다.
-// import React, { lazy, Suspense } from 'react';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Landing, EventList, AboutUs } from 'components';
-import {
-  ResultPageContainer, SelectPageContainer, MyPageContainer, FeedbackContainer,
-} from 'containers';
 
-// const Result = lazy(() => import('container/ResultPageContainer'));
-
+const Landing = lazy(() => import('components/Landing'));
+const EventList = lazy(() => import('components/EventList'));
+const AboutUs = lazy(() => import('components/AboutUs'));
+const ResultPageContainer = lazy(() => import('containers/ResultPageContainer'));
+const SelectPageContainer = lazy(() => import('containers/SelectPageContainer'));
+const MyPageContainer = lazy(() => import('containers/MyPageContainer'));
+const FeedbackContainer = lazy(() => import('containers/FeedbackContainer'));
 
 const Main = () => (
-  <>
+  <Suspense fallback={<Dody />}>
     <Route exact path="/" component={Landing} />
     <Route path="/selectTopping" component={SelectPageContainer} />
     <Switch>
@@ -23,8 +23,17 @@ const Main = () => (
     <Route path="/EventPage" component={EventList} />
     <Route path="/feedback" component={FeedbackContainer} />
     <Route path="/AboutUs" component={AboutUs} />
-  </>
+  </Suspense>
 );
+
+
+function Dody() {
+  return (
+    <div>
+      ddd
+    </div>
+  );
+}
 
 
 export default Main;
