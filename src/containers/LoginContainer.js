f9a68bacfa3b7bebede81053634a2fbf3b139e05
Q00 <jqyu.lee@gmail.com>
2019-12-13 01:57:44 +0900

    refactor: 🛠add KakaoLogin into LoginContainer

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import Login from 'components/Login';
+
+import KakaoLogin from 'react-kakao-login';
+
 
 export default function LoginContainer() {
+  function onSuccess(result) {
+    console.log(result);
+  }
+
+  function onFailure(result) {
+    alert('로그인 에러');
+    console.log(result);
+  }
   return (
     <div>
-      <Login />
+      <KakaoLogin
+        jsKey={process.env.REACT_APP_JS_KEY}
+        getProfile
+        onSuccess={(result) => onSuccess(result)}
+        onFailure={(result) => onFailure(result)}
+        buttonText="kakao"
+      />
     </div>
   );
 }
