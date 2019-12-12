1b0ded0844ac35b7acb0a9893b3f77441c73130d
Q00 <jqyu.lee@gmail.com>
2019-12-13 01:59:03 +0900

    feat: ⚡️ user store module

--- src/modules/user.js
+++ src/modules/user.js
@@ -2,25 +2,25 @@ import { createAction, handleActions } from 'redux-actions';
 
 // 액션타입 정의
 const LOGIN = 'user/LOGIN';
-
+const LOGIN_SUCCESS = 'user/login_success';
 
 // 액션생성함수 정의
-export const login = createAction(LOGIN);
+export const login = createAction(LOGIN, (user) => user);
+export const loginSuccess = createAction(LOGIN_SUCCESS);
 
 
 // 초기값
 const initialState = {
-  email: '',
-  nickname: '',
-  authenticate: '',
+  user: {},
 };
 
 export default handleActions({
-  [login]: (state, action) => {
-    const { isLogin } = action.payload;
-    return {
-      ...state,
-      isLogin,
-    };
-  },
+  [LOGIN]: (state, action) => ({
+    ...state,
+    user: action.payload,
+  }),
+  [LOGIN_SUCCESS]: (state, action) => ({
+    ...state,
+    user: action.payload,
+  }),
 }, initialState);
