8a42c57aac0973e776f1f664fe995f113f11e28d
Q00 <jqyu.lee@gmail.com>
2019-12-13 03:33:12 +0900

    fix: 🔥 use useSelector in AuthRoute

--- src/containers/AuthRouteContainer.js
+++ src/containers/AuthRouteContainer.js
@@ -1,16 +1,14 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { loginSuccess } from 'modules/user';
-import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export default function AuthRoute({
   ...props
 }) {
-  const dispatch = useDispatch();
-  const LoginSuccess = useCallback(() => dispatch((loginSuccess())), [dispatch]);
+  const { user } = useSelector((store) => store.user);
   const { path, component } = props;
   return (
-    LoginSuccess ? (
+    user.access_token !== undefined ? (
       <Route path={path} component={component} />)
       : (<Redirect to="/login" />)
   );
