af3a8ededa64020c6128de90c7481024f43a05b3
Q00 <jqyu.lee@gmail.com>
2019-12-13 01:58:37 +0900

    feat: ⚡️ save user in store when loginSuccess

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,11 +1,21 @@
-import React from 'react';
-
+import React, { useCallback } from 'react';
+import { login } from 'modules/user';
+import { useDispatch } from 'react-redux';
 import KakaoLogin from 'react-kakao-login';
 
 
 export default function LoginContainer() {
+  const dispatch = useDispatch();
+  const Login = useCallback((user) => dispatch((login(user))), [dispatch]);
   function onSuccess(result) {
+    // 스토어에 저장
     console.log(result);
+    Login({
+      access_token: result.response.access_token,
+      kakao_id: result.profile.id,
+      nickname: result.profile.kakao_account.profile.nickname,
+      image: result.profile.kakao_account.profile.profile_image_url,
+    });
   }
 
   function onFailure(result) {
