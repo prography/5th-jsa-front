cdf6a6a6c8d0d47e67d3d2823e52345305ee3fda
Q00 <jqyu.lee@gmail.com>
2019-12-16 18:05:44 +0900

    feat: ⚡️navcontainer에 handleLogout 추가

--- src/containers/NavContainer.js
+++ src/containers/NavContainer.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Nav } from 'components';
-import { useSelector } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from 'modules/user';
 
 export default function NavContainer() {
   const { user } = useSelector((store) => store.user);
   const isLogin = user.access_token !== undefined ? '마이페이지' : '로그인';
+  const dispatch = useDispatch();
+  const Logout = useCallback((() => dispatch((logout()))), [dispatch]);
+  function handleLogout() {
+    Logout();
+    localStorage.clear();
+  }
   return (
     <Nav
       isLogin={isLogin}
+      handleLogout={handleLogout}
     />
   );
 }
