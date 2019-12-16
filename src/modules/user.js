3a71eeb5dc54749744ffb77f8433a2dd0e827331
Q00 <jqyu.lee@gmail.com>
2019-12-16 18:05:10 +0900

    feat: ⚡️ logout action 추가

--- src/modules/user.js
+++ src/modules/user.js
@@ -3,10 +3,12 @@ import { createAction, handleActions } from 'redux-actions';
 // 액션타입 정의
 const LOGIN = 'user/LOGIN';
 const LOGIN_SUCCESS = 'user/login_success';
+const LOGOUT = 'user/LOGOUT';
 
 // 액션생성함수 정의
 export const login = createAction(LOGIN, (user) => user);
 export const loginSuccess = createAction(LOGIN_SUCCESS);
+export const logout = createAction(LOGOUT);
 
 
 // 초기값
@@ -22,5 +24,9 @@ export default handleActions({
   [LOGIN_SUCCESS]: (state, action) => ({
     ...state,
     user: action.payload,
+  }),
+  [LOGOUT]: (state) => ({
+    ...state,
+    user: {},
   }),
 }, initialState);
