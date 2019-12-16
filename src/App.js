702bd1392baf68d7b4343b09e72baaa2f9bcfb19
Q00 <jqyu.lee@gmail.com>
2019-12-16 18:50:37 +0900

    feat:  ⚡️첫 렌더링 시 로컬스토리지 로그인상태 검사

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // main을 데려고오고 네브바 등 fixed 되어있는걸 가져옵니다
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { login } from 'modules/user';
 import { BrowserRouter } from 'react-router-dom';
 import { Footer } from 'components';
 import HTML5Backend from 'react-dnd-html5-backend';
@@ -8,6 +10,20 @@ import Main from './containers/Main';
 import { NavContainer } from './containers';
 
 function App() {
+  const dispatch = useDispatch();
+  const Login = useCallback((user) => dispatch((login(user))), [dispatch]);
+
+  const userInfo = localStorage.getItem('userInfo');
+  const user = userInfo !== null ? JSON.parse(userInfo) : ({});
+  // console.log('user', user);
+  // userInfo를 localStorage에서 검사하고 없으면 store에도 없으므로, localStore에서 받아온 값을 store에 추가
+  if (Object.entries(user).length !== 0) {
+    Login({
+      access_token: user.access_token,
+      kakao_id: user.kakao_id,
+      nickname: user.nickname,
+    });
+  }
   return (
     <DndProvider backend={HTML5Backend}>
       <BrowserRouter>
