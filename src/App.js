946c96a225bd5196cd46000cc37f41bd14cf872e
Dody Kang <kangdoroth2500@gmail.com>
2019-12-30 21:43:42 +0900

    refactor: 🛠 레거시코드 정리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // main을 데려고오고 네브바 등 fixed 되어있는걸 가져옵니다
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { login } from 'modules/user';
 import { BrowserRouter } from 'react-router-dom';
 import { Footer, Nav, Snackbar } from 'components';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DndProvider } from 'react-dnd';
-import { useSelector } from 'react-redux';
+
 import Main from './containers/Main';
 import { NavContainer } from './containers';
 
