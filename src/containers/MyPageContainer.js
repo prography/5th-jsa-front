c03f323e3fe4e06972d742867f0e9e78a7b588db
승가리 <imsa1224@korea.ac.kr>
2020-01-14 21:12:54 +0900

    add myPageContainer

--- src/containers/MyPageContainer.js
+++ src/containers/MyPageContainer.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { MyPage } from 'components';
+import * as api from 'lib/api';
+import { login } from 'modules/user';
 
 export default function MyPageContainer() {
+  const [user, setUser] = useState([]);
+  const getToken = localStorage.getItem('userInfo');
+  console.log(getToken);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        api.myPageMain(getToken).then(res => {
+          console.log(res);
+          setUser(res.data);
+        });
+      } catch (e) {}
+    };
+    fetchUser();
+  }, []);
+  console.log(user);
   return (
     <div>
-      <MyPage />
+      <MyPage user={user} />
     </div>
   );
 }
