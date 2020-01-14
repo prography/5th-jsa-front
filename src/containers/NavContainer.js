91bdf90b048eb13e1841ade7c002bafa90accdfe
Dody Kang <kangdoroth2500@gmail.com>
2020-01-14 20:46:52 +0900

    refactor: 🛠 fixed login handler

--- src/containers/NavContainer.js
+++ src/containers/NavContainer.js
@@ -1,16 +1,42 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Nav } from 'components';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout, login } from 'modules/user';
 import { showSnackbar } from 'modules/snackbar';
+import { openLoginDialog, closeLoginDialog } from 'modules/dialog';
 import { withRouter } from 'react-router-dom';
 import * as api from 'lib/api';
 
 const NavContainer = ({ history }) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
   const { isLogin } = useSelector((store) => store.user);
+  const { loginDialogOpen } = useSelector((store) => store.dialog);
   const dispatch = useDispatch();
   const Logout = useCallback((() => dispatch((logout()))), [dispatch]);
+  const Login = useCallback((user) => dispatch((login(user))), [dispatch]);
+  const OpenLoginDialog = useCallback((user) => dispatch((openLoginDialog(user))), [dispatch]);
+  const CloseLoginDialog = useCallback((user) => dispatch((closeLoginDialog(user))), [dispatch]);
   const ShowSnackbar = useCallback((list) => dispatch((showSnackbar(list))), [dispatch]);
+
+  useEffect(() => {
+    const checkToken = localStorage.getItem('userInfo');
+    if (checkToken) {
+      api.getSigninCheck(checkToken)
+        .then((res) => {
+          if (res.data.login === 'true') {
+            Login({
+              isLogin: true,
+              userInfo: {
+                accessToken: res.data.user._id,
+                kakaoId: res.data.user.kakao,
+                nickname: res.data.user.nickname,
+                image: res.data.user.profile_image,
+              },
+            });
+          }
+        });
+    }
+  }, []);
 
   function handleLogout() {
     Logout({ isLogin: false });
@@ -18,11 +44,8 @@ const NavContainer = ({ history }) => {
     return history.push('/');
   }
 
-  const Login = useCallback((user) => dispatch((login(user))), [dispatch]);
-
   function onSuccess(result) {
     // 스토어에 저장
-    console.log(result);
     Login({
       isLogin: true,
       userInfo: {
@@ -34,14 +57,9 @@ const NavContainer = ({ history }) => {
     });
     api.getKakaoSignin(result.response.access_token)
       .then((res) => {
-        // 카카오 로그인하고, 토큰을 받는데 DB에 아이디 값이 없을 경우 저장 후 토큰 전달, 있으면 토큰 전달
-        // 그러면 로컬스토리지에는 이 response의 토큰 값이 들어가게 되는게 맞나요?
-        localStorage.setItem('userInfo',
-          JSON.stringify({
-            accessToken: res.data.token,
-          }));
+        localStorage.setItem('userInfo', res.data.token);
+        ShowSnackbar({ content: '로그인 성공!' });
       });
-    ShowSnackbar({ content: '로그인 성공!' });
   }
 
   function onFailure(result) {
@@ -54,6 +72,11 @@ const NavContainer = ({ history }) => {
       handleLogout={handleLogout}
       onSuccess={onSuccess}
       onFailure={onFailure}
+      dialogOpen={dialogOpen}
+      setDialogOpen={setDialogOpen}
+      OpenLoginDialog={OpenLoginDialog}
+      CloseLoginDialog={CloseLoginDialog}
+      loginDialogOpen={loginDialogOpen}
     />
   );
 };
--- src/modules/user.js
+++ src/modules/user.js
@@ -3,16 +3,23 @@ import { createAction, handleActions } from 'redux-actions';
 // 액션타입 정의
 const LOGIN = 'user/LOGIN';
 const LOGOUT = 'user/LOGOUT';
+const UPDATE_TOKEN = 'user/UPDATE_TOKEN';
 
 // 액션생성함수 정의
 export const login = createAction(LOGIN, (userInfo) => userInfo);
 export const logout = createAction(LOGOUT);
+export const updateToken = createAction(UPDATE_TOKEN);
 
 
 // 초기값
 const initialState = {
   isLogin: false,
-  userInfo: {},
+  userInfo: {
+    kakaoId: undefined,
+    accessToken: undefined,
+    nickname: undefined,
+    image: undefined,
+  },
 };
 
 export default handleActions({
@@ -25,5 +32,12 @@ export default handleActions({
     ...state,
     isLogin: action.payload,
     userInfo: {},
+  }),
+  [UPDATE_TOKEN]: (state, action) => ({
+    ...state,
+    userInfo: {
+      ...state.userInfo,
+      accessToken: action.payload,
+    },
   }),
 }, initialState);
